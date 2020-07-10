5f6c2d0c6260d6c4c4bca7fd1fb8abfb8a60830e
rohit <57873071+rohit0452@users.noreply.github.com>
2020-07-10 16:00:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todolist from './todoitem';
+import Header from "./header"
+import Footer from "./footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        title: "",
+        description: "",
+        deadline: "",
+        TodoItems: [
+            {
+                id: 1,
+                title: "React",
+                description: "Description",
+                deadline: "Deadline",
+            },
+            {
+                id: 2,
+                title: "JavaScript",
+                description: "Description",
+                deadline: "Deadline",
+            }
+        ]
+
+    }
+    render() {
+
+        var todoItemJSX = this.state.TodoItems.map((item) =>
+            <Todolist title={item.title} description={item.description} deadline={item.deadline} />
+        )
+
+        return (
+            <div>
+                  <Header/>
+            <div className="App">
+                 
+                <div className="todolist">
+                  
+                    {todoItemJSX}
+                </div>
+
+                <div className="addinput">
+                    <input className="inputbox" type="text" placeholder="Enter Title"></input>
+                     <br></br>
+                    <input className="inputbox" type="text" placeholder="Enter Description"></input>
+                    <br></br>
+                    <input className="inputbox" type="text" placeholder="Enter Deadline"></input>
+                    <br></br>
+                    <button id="addtaskbtn">Add New Task</button>
+                </div>
+
+            </div>
+            <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
